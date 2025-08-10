cfa5849e262b7ab91575645a16f172c4d67cd2cf
里安 <lienyu@brmyx.com>
2025-08-10 17:02:53 +0800

    数羊（Lambda）

--- Program.cs
+++ Program.cs
@@ -63,6 +63,7 @@ public class Program
 }
 public class Kata
 {
+    public static int CountSheeps(bool[] sheeps) => sheeps.Count(x => x);
     // In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater than or equal to p = 1200 inhabitants?
     //     More generally given parameters:
 
